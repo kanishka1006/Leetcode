e369bb41b935cb4b16f2c2f571b1e2fc9281dae2
Kanishka Suri <2024a1r024@mietjammu.in>
2026-07-13 14:09:00 +0530

    Add LeetCode solutions

---
+++ 9_Palindrome_Number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+       
+        """
+        :type x: int
+        :rtype: bool
+        """
+  
+        original = x
+        rev = 0
+        while x >0:
+            digit = x % 10
+            rev  = rev *10 +digit
+            x = x //10
+        if original == rev:
+            return True
+        return False
